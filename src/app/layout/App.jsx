f0bf777c0ce3ed22410c9a6bf801d2c24ee60b4d
Godsgift ThankGod Akari <giftakari@yahoo.com>
2020-04-27 16:53:03 +1000

    application layout"

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
 
 export default class App extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <button className="ui icon button">
+          <i className="smile icon"></i>
+          Css Button
+        </button>{" "}
+        <Button icon="smile" content="Akaris" />
+      </div>
+    );
   }
 }
